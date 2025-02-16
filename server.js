5f3a43b6c0c99d2f58f79e11bfc7fc8d7f70de75
TEJASPATIL0710 <tejaspatil.02005@gmail.com>
2025-02-16 20:11:15 +0530

    mern app server and dotenv setup done

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ const PORT = process.env.PORT || 8080;
 
 // Run Listen
 app.listen(PORT,() => {
-    console.log(`Server running on ${Dev_Mode} mode on port ${PORT}`.bgCyan.white);
+    console.log(`Server running on ${process.env.Dev_Mode} mode on port ${PORT}`.bgCyan.white);
 })
